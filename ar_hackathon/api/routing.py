d21ed40d0968067f340e9001d03afd0c8d11d8df
Avah Xiao <avahxiao@gmail.com>
2026-09-15 15:12:03 -0700

    Add team info

--- ar_hackathon/api/routing.py
+++ ar_hackathon/api/routing.py
@@ -5,8 +5,8 @@ This module defines the routing API for the Amazon Robotics Hackathon.
 Students will implement the drive_unit_next_move function in this module.
 
 *****IMPORTANT*****
-Team name:
-Email address:
+Team name: Short Eric
+Email address: xaviergbradford@gmail.com, eric.yoon4@gmail.com, avahxiao@gmail.com
 *******************
 """
 
